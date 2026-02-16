9e020043dfc00f99e953a7aadcdf8adb54b5b5bc
Ashwath Prashant Narekuli <aswath.prashant.narekuli.ext@adbsafegate.com>
2026-02-16 09:50:04 +0100

    init

---
+++ Functions.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 16 09:28:35 2026
+
+@author: aprash
+"""
+
+import matplotlib.pyplot as plt
+
+def show_img(image):
+    
+    plt.imshow(image)
+    plt.axis('off')
+    plt.show()
