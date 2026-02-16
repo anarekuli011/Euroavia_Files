b939317ea238823251f577fbbf570b3b14dd126e
Ashwath Prashant Narekuli <aswath.prashant.narekuli.ext@adbsafegate.com>
2026-02-16 15:19:47 +0100

    Added zoom feature in the show_img function

--- Functions.py
+++ Functions.py
@@ -8,9 +8,17 @@ Created on Mon Feb 16 09:28:35 2026
 import matplotlib.pyplot as plt
 import cv2
 
-def show_img(path):
+def show_img(path, zoom_factor=1.0):
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
-    image = cv2.imread(path)    
+    if zoom_factor < 1.0:
+        h, w, _ = image.shape
+        dy = int(h * (1 - zoom_factor) / 2)
+        dx = int(w * (1 - zoom_factor) / 2)
+        
+        image = image[dy:h-dy, dx:w-dx]
+
     plt.imshow(image)
     plt.axis('off')
     plt.show()
