af77191db6938799cea636f0c8b1a7064344e02d
Ashwath Prashant Narekuli <aswath.prashant.narekuli.ext@adbsafegate.com>
2026-02-17 09:15:08 +0100

    Added enlarge feature in the show_img function

--- Functions.py
+++ Functions.py
@@ -8,7 +8,7 @@ Created on Mon Feb 16 09:28:35 2026
 import matplotlib.pyplot as plt
 import cv2
 
-def show_img(path):
+def show_img(path, x=20, y=12):
     image = cv2.imread(path)
     
     if image is None:
@@ -17,7 +17,7 @@ def show_img(path):
 
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    plt.figure(figsize=(20, 12)) 
+    plt.figure(figsize=(x, y)) 
     
     plt.imshow(image)
     plt.axis('off')
